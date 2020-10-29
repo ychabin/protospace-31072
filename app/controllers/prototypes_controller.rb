66ef7615e8c1c217a604eb0c27d7716ecf8542a0
ychabin <chabin043@icloud.com>
2020-10-29 12:24:23 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,66 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :authenticate_user!, except: [ :index]
+  before_action :move_to_index, except: [ :index, :show, :edit]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    if current_user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    #@prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    if user_signed_in? && current_user.id
+      redirect_to action: :index
+    end
+    #@prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :update
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    if prototype.destroy
+       redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:nickname, :title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+end
